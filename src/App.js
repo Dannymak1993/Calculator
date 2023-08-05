ce96398a06096e5377b2e3e759fec66fe89fd9c1
danny-mak1993 <danny.mak2@gmail.com>
2023-08-05 17:42:44 -0500

    added multiple divs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 function App() {
   return (
-    <div className="calculator-grid"></div>
+    <div className="calculator-grid">
+      <div className="output">
+        <div className="previous-operand"></div>
+        <div className="current-operand"></div>
+      </div>
+    </div>
   )
 }
 
